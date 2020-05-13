c0bcd019bc6b36cb885e9946f6581d942ea4a71a
spurthi ganapathineedi <sgana@ibm.com>
2020-05-13 17:18:02 -0500

    Updated the json name of the attribute parameters

--- instances.go
+++ instances.go
@@ -50,11 +50,11 @@ type PolicyData struct {
 // Attributes contains the detals of allowed network policy type
 type Attributes struct {
 	AllowedNetwork      string `json:"allowed_network,omitempty"`
-	CreateRootKey       *bool  `json:"create_root_key,omitempty"`
-	CreateStandardKey   *bool  `json:"create_standard_key,omitempty"`
-	ImportRootKey       *bool  `json:"import_root_key,omitempty"`
-	ImportStandardKey   *bool  `json:"import_standard_key,omitempty"`
-	SecureImportRootKey *bool  `json:"secure_import_root_key,omitempty"`
+	CreateRootKey       *bool  `json:"create_root_key_material,omitempty"`
+	CreateStandardKey   *bool  `json:"create_standard_key_material,omitempty"`
+	ImportRootKey       *bool  `json:"import_root_key_material,omitempty"`
+	ImportStandardKey   *bool  `json:"import_standard_key_material,omitempty"`
+	SecureImportRootKey *bool  `json:"secure_import_root_key_material,omitempty"`
 }
 
 // InstancePolicies represents a collection of Policies associated with Key Protect instances.
